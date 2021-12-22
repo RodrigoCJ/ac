aa455b965d19886b91c469a1106f08e780a8b889
Rodrigo <rodrigocj>
2021-12-22 12:11:59 -0300

    gif

--- dist/index.js
+++ dist/index.js
@@ -15260,7 +15260,7 @@ async function run() {
     const octokit = github.getOctokit(GITHUB_TOKEN);
     const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
-    const randomPos = Math.round(Math.random() * 1000);
+    const randomPos = 1;
     const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
     const response = await fetch(url);
     const { results } = await response.json();
@@ -15272,7 +15272,7 @@ async function run() {
     await octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
-        body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />    '
+        body: `Thank you for submitting a pull request! We will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`
       });
   }
   
