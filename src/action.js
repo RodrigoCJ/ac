beadc2c89f2d10ec7aeb3d13eeae782af155cc2d
Rodrigo <rodrigocj>
2021-12-23 16:05:51 -0300

    implementacao trello

--- src/action.js
+++ src/action.js
@@ -12,7 +12,7 @@ async function run() {
     var results;
 
     do{
-        const randomPos = Math.round(Math.random() * 100);;
+        const randomPos = Math.round(Math.random() * 10000);;
         const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
         const response = await fetch(url);
         results = await response.json();
@@ -23,11 +23,21 @@ async function run() {
     const { context = {} } = github;
     const { pull_request } = context.payload;
 
-    await octokit.rest.issues.createComment({
+    await commenta();
+}
+
+function commenta(){
+    var nome = "Rodrigo";
+    var msg = "teste";
+
+    octokit.rest.issues.createComment({
         ...context.repo,
         issue_number: pull_request.number,
-        body: `Obrigado pela pull request, vamos analiza-la o mais rapido possivel.\n\n<img src="${gifUrl}" alt="thank you" />`
-      });
-  }
-  
-  run();
+        body: `Obrigado pela pull request, vamos analiza-la o mais rapido possivel.\n\n<img src="${gifUrl}" alt="obrigado" />`  
+    });
+
+    const url_trellourl = `https://api.trello.com/1/cards?key=${TRELLO_KEY}&token=${TRELLO_TOKEN}&name=${nome}&desc=${msg}&idList=${TRELLO_IDLIST}`;
+    fetch(url_trello, {method: 'POST',headers: {'Accept': 'application/json'}});
+    console.log("API");
+}
+run();
